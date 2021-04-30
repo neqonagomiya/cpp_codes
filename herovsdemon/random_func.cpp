bd8ec8aac72b50591a437e8e9e5602e1bf3dcd39
neqonagomiya <neqofan@gmail.com>
2021-04-30 18:16:10 +0900

    herovsdemon

---
+++ herovsdemon/random_func.cpp
@@ -0,0 +1,10 @@
+//random test example
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+  cout << rand() << endl;
+  cout << rand() << endl;
+  cout << rand() << endl;
+}
